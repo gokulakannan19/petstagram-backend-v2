97048efe6a16aa5308c334ec7743cc19c0179631
gokulakannan19 <goks.cloud19@gmail.com>
2026-04-04 22:06:16 +0530

    Add configuration and database setup for application

---
+++ app/core/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from app.core.config import settings
+
+
+engine = create_engine(settings.DATABASE_URL)
+
+sessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+base = declarative_base()
+
