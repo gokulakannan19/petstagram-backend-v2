731a64f1489ce5b35e131f401abcf5d253913bf4
gokulakannan19 <goks.cloud19@gmail.com>
2026-04-05 14:14:59 +0530

    add oauth2 security

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,4 +1,5 @@
 from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 from starlette import status
 from sqlalchemy.orm import Session
 from jose import jwt, JWTError
@@ -6,6 +7,9 @@ from jose import jwt, JWTError
 from app.core.config import settings
 from app.core.database import sessionLocal
 from app.models.user import User
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 
 def get_db():
@@ -16,13 +20,23 @@ def get_db():
         db.close()
 
 
-def get_current_user(token: str = Depends(...), db: Session = Depends(get_db)):
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
+):
     try:
         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
         user_id = payload.get("sub")
+
+        if user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+
     except JWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
 
     user = db.query(User).filter(User.id == int(user_id)).first()
-    return user
 
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+
+    return user
