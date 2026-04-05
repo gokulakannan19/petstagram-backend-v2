d5279a64f830ad25243d62385935dc81a5d82bf7
gokulakannan19 <goks.cloud19@gmail.com>
2026-04-05 13:58:18 +0530

    Implement JWT authentication with login endpoint and user login service

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,7 +1,7 @@
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from app.repository.user_repo import get_user_by_email, create_user
-from app.core.security import hash_password
+from app.core.security import create_access_token, hash_password, verify_password
 
 
 def register_user(db: Session, email: str, password: str):
@@ -12,3 +12,18 @@ def register_user(db: Session, email: str, password: str):
     hashed_password = hash_password(password)
     return create_user(db, email, hashed_password)
 
+
+def login_user(db, email, password):
+    user = get_user_by_email(db, email)
+    if not user:
+        raise HTTPException(status_code=400, detail="Invalid email or password")
+    
+    if not verify_password(password, user.password):
+        raise HTTPException(status_code=400, detail="Invalid email or password")
+
+    token = create_access_token({"sub": str(user.id)})
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
